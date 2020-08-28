28c8ce33d522244f7854d37d88984b705e9e2057
AzureQ <65605194+Azure-Q@users.noreply.github.com>
2020-08-28 16:20:52 +0800

    Update extractPage.test.js

--- file_upload/__tests__/extractPage.test.js
+++ file_upload/__tests__/extractPage.test.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
 import Enzyme, {shallow,render,mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import AppList from '../src/commonComponents/appList';        //待测试文件的路径；
+import ExtractPage from '../src/jsPages/fileExtract/extractPage';        //待测试文件的路径；
 
 Enzyme.configure({adapter: new Adapter()});
 
 //整体测试；
-describe('AppList test', function () {
-    it('Test for handleCancel', function(){
+describe('ExtractPage test', function () {
+    it('Test for all functions', function(){
         const props = {
-            setSelectedApp:jest.fn()
+            formRef:React.createRef()
           };
-        let wrapper = shallow(<AppList {...props} />);
-        
-        wrapper.find('Modal').simulate('cancel');
-        expect(props.setSelectedApp).toHaveBeenCalled();
 
-        wrapper.find('li').at(0).simulate('click');
-        expect(props.setSelectedApp).toHaveBeenCalled();
+        let wrapper = shallow(<ExtractPage {...props} />);
+        expect(wrapper.find("Button").at(0).text()).toEqual("Submit");
+        expect(wrapper.find("Button").at(1).text()).toEqual("Reset");  
 
-        wrapper.find('li').at(1).simulate('click');
-        expect(props.setSelectedApp).toHaveBeenCalled();
+        wrapper.instance().openApp();
+        expect(wrapper.instance().state.visible).toEqual(true);
 
-        wrapper.find('li').at(2).simulate('click');
-        expect(props.setSelectedApp).toHaveBeenCalled();
+        wrapper.instance().showFileContent("text/test.txt");
 
-        wrapper.find('li').at(3).simulate('click');
-        expect(props.setSelectedApp).toHaveBeenCalled();
-    });
+        wrapper.find('Button').at(0).simulate('click');
+        wrapper.find('Button').at(1).simulate('click');
+  });
+
+  it('Test for handleSubmit', function(){
+    const props = {
+        setVisibility:jest.fn(),
+        fileContent:"Hello cirix!"
+      };
+
+    let wrapper = shallow(<ExtractPage {...props} />);
+    
+    wrapper.find('Button').at(0).simulate('click');
+    expect(wrapper.instance().state.visible).toEqual(false);
+
+    wrapper.find('AppList').simulate('setSelectedApp');
+    expect(wrapper.instance().state.visible).toEqual(false);
+  });
 });
